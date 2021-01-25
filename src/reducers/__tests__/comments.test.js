eaf65386a6024d6ec54901128599c8a0d38c4873
Simon Jacquemin <64366026+Echoes-88@users.noreply.github.com>
2021-01-25 17:30:08 +0100

    Update comments.test.js

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -12,8 +12,7 @@ it('handles actions of type SAVE_COMMENT', () => {
     expect(newState).toEqual(['New comment']);
 })
 
-// Le test suivant permet de vérifier que le reducer fonctionne bien de manière global
-// Si on effectue le test précedant, ce test n'est pas très utile
+// Global testing of reducer
 it('handles action with unknown type', () => {
     const newState = commentsReducer([], { type: undefined})
 
